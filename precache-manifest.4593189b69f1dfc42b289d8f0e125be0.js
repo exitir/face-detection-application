1b4221cabd3a1a02f91c15bc1307d828cfd39133
exitir@gmail.com <exitir@gmail.com>
2020-07-13 02:27:58 +0530

    Updates

---
+++ precache-manifest.4593189b69f1dfc42b289d8f0e125be0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc7d64570923fe45d0144d392a1bc34d",
+    "url": "/exitir/face-detection-application/index.html"
+  },
+  {
+    "revision": "4ebe5eb50326086f53ba",
+    "url": "/exitir/face-detection-application/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9f5ecb5242f42e5fbe5e",
+    "url": "/exitir/face-detection-application/static/css/main.6dac85b7.chunk.css"
+  },
+  {
+    "revision": "4ebe5eb50326086f53ba",
+    "url": "/exitir/face-detection-application/static/js/2.bffb4b8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exitir/face-detection-application/static/js/2.bffb4b8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f5ecb5242f42e5fbe5e",
+    "url": "/exitir/face-detection-application/static/js/main.d098b8dc.chunk.js"
+  },
+  {
+    "revision": "d2a22da9f9090098804b",
+    "url": "/exitir/face-detection-application/static/js/runtime-main.e24aa572.js"
+  }
+]);
